b9fd3dac26e731368f02cccdf6c7e1ab0374f377
dfergusbrown <dfergusbrown@gmail.com>
2023-09-30 22:28:59 -0400

    fonts and positioning

--- src/sections/CoverPage.js
+++ src/sections/CoverPage.js
@@ -1,24 +1,27 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { ReactComponent as Logo} from '../assets/arrow.svg'
 
 const CoverPage = () => {
 
     return (
         <Container className="backgroundCover">
-            <Row style={{
-            }}>
+            <Row>
                 <h2>Fergus Brown</h2>
                 <hr/>
             </Row>
             <Row className="developerHeading">
                 <Col><h1>Web Developer</h1></Col>
             </Row>
-            <Row>
-                <h3>Check out my projects</h3>
+            <Row id="lastrow">
+                <Col id="cantedLineBox">
+                    <h3>Check out my projects</h3>
+                </Col>
+                <Col id="arrowbox">
+                    <Logo />
+                </Col>
             </Row>
         </Container>
     )
 }
-
-
 
 export default CoverPage;
