c576753e9cbe10822dc66018500795a91ae44994
dfergusbrown <dfergusbrown@gmail.com>
2024-06-03 15:48:31 -0400

    animates project background - controlled via parallax scrolling

--- src/components/ProjectBackground.jsx
+++ src/components/ProjectBackground.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Parallax, useParallax } from 'react-scroll-parallax';
 
 const ProjectBackground = () => {
-    const projArray = Array(50).fill('Projects')
-    const projMatrix = Array(50).fill(projArray)
+    const projArray = Array(500).fill('Projects')
+    const projMatrix = Array(100).fill(projArray)
+    // const parallax = useParallax<HTMLDivElement>({
+    //     rotate: [0, 360],
+    //   });
   return (
     <div className='headingBackground'>
         {
             projMatrix.map((el, idx) => (
-                <div key={idx}>{el.map((elem, index) => (
+                <Parallax key={idx} translateX={idx%2 === 0 ? ["-200px", "200px"] : ["200px", "-200px"]} className={idx%2 === 0 ? "evenRow" : "oddRow"}>{el.map((elem, index) => (
                     <span key={index}>{elem}</span>
-                ))}</div>
+                ))}</Parallax>
             ))
         }
     </div>
